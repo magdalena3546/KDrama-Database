9c52bc4bdd91cc28b85fe555d3f19d4209e65454
magdalena3546 <magdalenamalek52@gmail.com>
2024-04-22 19:52:53 +0200

    clear data, create database in mysql, load data into table, create script.py to change time(string) to minutes(int)

---
+++ Kdramas/queres/creating_database.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE kdramaDB;
+
+USE kdramaDB;
+
+-- create table with kdrama info
+CREATE TABLE kdramaData (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	kdramaName VARCHAR(255) NOT NULL,
+	yearOfRelease INT NOT NULL,
+    airedDate VARCHAR(100),
+    airedOn VARCHAR(80),
+    numOfEpisode INT,
+    network VARCHAR(50),
+    duration INT,
+	contentRating VARCHAR(100),
+    synopsis VARCHAR(800),
+    cast VARCHAR(500),
+    genre VARCHAR(80),
+    tags VARCHAR(255),
+    ranks VARCHAR(50),
+    rating DECIMAL(3, 1)
+);
+-- load data into kdrama table
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\kdrama.csv'
+INTO TABLE kdramaData
+CHARACTER SET utf8
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(kdramaName, yearOfRelease, airedDate, airedOn, numOfEpisode, network, duration, contentRating, synopsis, cast, genre, tags, ranks, rating);
+
+SELECT * FROM kdramadata;
+
+
+
+
+
+
+
---
+++ Kdramas/script.py
@@ -0,0 +1,33 @@
+import csv
+import re
+
+def timeToMinutes(time):
+    time = time.strip()
+    time = re.sub(r'(\d+)\s+(hr\.|min\.)', r'\1\2', time)
+    parts = time.split()
+    minutes = 0
+    for part in parts:
+        if 'hr.' in part:
+            hours = part.replace('hr.', '')
+            if hours.isdigit():
+                minutes += int(hours) * 60
+        elif 'min.' in part:
+            minutes_str = part.replace('min.', '')
+            if minutes_str.isdigit(): 
+                minutes += int(minutes_str)
+    print(minutes)
+    return minutes
+
+with open('./csv/top100_kdrama.csv', 'r', encoding="utf-8") as csvfile:
+    kdramaDB = csv.DictReader(csvfile)
+    converted_data = []
+    fieldnames = kdramaDB.fieldnames
+    for row in kdramaDB:
+        minutes = timeToMinutes(row['Duration'])
+        row['Duration'] = minutes
+        converted_data.append(row)
+        
+with open('./csv/top100_kdrama.csv', 'w', encoding="utf-8") as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(converted_data)
